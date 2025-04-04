0c48111c025934f82d93dda051f7655ea7bb0364
ArunShrestha <arunstha547@gmail.com>
2025-04-05 00:29:24 +0545

    Add functionality to disable extension icon and actions on Google domains. Implemented a domain check in background.js and updated manifest.json to exclude various Google domains from extension interactions.

--- background.js
+++ background.js
@@ -1,6 +1,32 @@
 // background.js
+function isGoogleDomain(url) {
+    return url.match(/^https?:\/\/([^\/]+\.)?google\.(com|co\.[a-z]{2,})\/.*$/i);
+}
+
+// Disable extension icon on Google domains
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.url || changeInfo.status === 'complete') {
+        chrome.action.setIcon({
+            path: {
+                "16": "images/icon16.png",
+                "48": "images/icon48.png",
+                "128": "images/icon128.png"
+            },
+            tabId: tabId
+        });
+
+        chrome.action.setEnabled({
+            tabId: tabId,
+            enabled: !isGoogleDomain(tab.url)
+        });
+    }
+});
+
+// Existing click handler
 chrome.action.onClicked.addListener((tab) => {
-    chrome.tabs.sendMessage(tab.id, { action: "toggle_sidebar" });
+    if (!isGoogleDomain(tab.url)) {
+        chrome.tabs.sendMessage(tab.id, { action: "toggle_sidebar" });
+    }
 });
 
 // Listen for the keyboard shortcut
